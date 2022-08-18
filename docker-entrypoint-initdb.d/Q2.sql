7a957723cff9a58f4c91d9f706df119146340be0
cozy-corner <koji.sasaki@sansan.com>
2022-08-18 23:15:05 +0900

    q2

--- docker-entrypoint-initdb.d/Q2.sql
+++ docker-entrypoint-initdb.d/Q2.sql
@@ -10,12 +10,17 @@ CREATE TABLE ExcuseList
 CREATE TABLE Absenteeism (
   emp_id INTEGER NOT NULL , 
   absent_date DATE NOT NULL, 
-  reason_code CHAR (40) NOT NULL , 
+  reason_code CHAR (40) NOT NULL REFERENCES excuselist(reason_code), 
   severity_points INTEGER NOT NULL CHECK (severity_points BETWEEN 0 AND 4), 
   PRIMARY KEY (emp_id, absent_date)); 
 
 INSERT INTO Personnel VALUES(1);
 INSERT INTO Personnel VALUES(2);
+
+INSERT INTO ExcuseList VALUES('ずる');
+INSERT INTO ExcuseList VALUES('病気');
+INSERT INTO ExcuseList VALUES('ケガ');
+INSERT INTO ExcuseList VALUES('サボリ');
 
 INSERT INTO Absenteeism VALUES(1, '2007-05-01', 'ずる', 4);
 INSERT INTO Absenteeism VALUES(1, '2007-05-02', '病気', 2);   --0になる
